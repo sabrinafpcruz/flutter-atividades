bc9514d12beefef514addf508c4cb35984934610
Sabrina Cruz <sabrinafpcruz@gmail.com>
2024-10-02 11:57:32 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,21 +1,278 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ContadorApp());
+  runApp(MyApp());
 }
 
-class ContadorApp extends StatelessWidget {
-  const ContadorApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ContadorPage(),
+      home: HomeScreen(),
     );
   }
 }
 
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  // Lista de telas
+  final List<Widget> _screens = [ContadorPage(), GameScreen()];
+
+  // Função para alternar entre as telas
+  void _onMenuItemSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+      Navigator.pop(context); // Fecha o Drawer após a seleção
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_selectedIndex == 0 ? 'Projeto Contador' : 'Jogo da Velha'),
+      ),
+      body: _screens[_selectedIndex], // Exibe a tela selecionada
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.countertops),
+              title: Text('Contador'),
+              onTap: () => _onMenuItemSelected(0), // Seleciona a página do contador
+            ),
+            ListTile(
+              leading: Icon(Icons.grid_view),
+              title: Text('Jogo da Velha'),
+              onTap: () => _onMenuItemSelected(1), // Seleciona a página do jogo da velha
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Código do Jogo da Velha
+class GameScreen extends StatefulWidget {
+  @override
+  _GameScreenState createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  List<String> board = ['', '', '', '', '', '', '', '', ''];
+  bool xTurn = true;
+  String currentPlayer = "X";
+  int xWins = 0;
+  int oWins = 0;
+  final int winningScore = 9;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jogo da Velha"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Jogador atual: $currentPlayer',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Placar: X: $xWins - O: $oWins',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemCount: 9,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    _markBoard(index);
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: Center(
+                      child: Text(
+                        board[index],
+                        style: TextStyle(
+                          color: board[index] == "X"
+                              ? Colors.red
+                              : board[index] == "O"
+                                  ? Colors.blue
+                                  : Colors.black,
+                          fontSize: 48,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _resetGame,
+            child: Text("Reiniciar o jogo"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _markBoard(int index) {
+    if (board[index] == '') {
+      setState(() {
+        board[index] = xTurn ? "X" : "O";
+        xTurn = !xTurn;
+        currentPlayer = xTurn ? "X" : "O";
+        _checkWinner();
+      });
+    }
+  }
+
+  void _checkWinner() {
+    List<List<int>> winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (var pattern in winPatterns) {
+      String a = board[pattern[0]];
+      String b = board[pattern[1]];
+      String c = board[pattern[2]];
+
+      if (a == b && b == c && a != '') {
+        _showWinnerDialog(a);
+        if (a == 'X') {
+          xWins++;
+          if (xWins == winningScore) {
+            _showGameWinnerDialog('X');
+          }
+        } else {
+          oWins++;
+          if (oWins == winningScore) {
+            _showGameWinnerDialog('O');
+          }
+        }
+        return;
+      }
+    }
+
+    if (!board.contains('')) {
+      _showDrawDialog();
+    }
+  }
+
+  void _showWinnerDialog(String winner) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Vitória!'),
+        content: Text('$winner ganhou esta rodada!'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              _resetBoard();
+            },
+            child: Text('OK'),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _showDrawDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Empate!'),
+        content: Text('Ninguém ganhou esta rodada.'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              _resetBoard();
+            },
+            child: Text('OK'),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _showGameWinnerDialog(String gameWinner) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Vencedor do jogo!'),
+        content: Text('$gameWinner venceu o jogo com 9 vitórias!'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              _resetGame();
+            },
+            child: Text('OK'),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _resetBoard() {
+    setState(() {
+      board = ['', '', '', '', '', '', '', '', ''];
+      xTurn = true;
+      currentPlayer = "X";
+    });
+  }
+
+  void _resetGame() {
+    setState(() {
+      xWins = 0;
+      oWins = 0;
+      _resetBoard();
+    });
+  }
+}
+
+// Código do Contador
 class ContadorPage extends StatefulWidget {
   @override
   _ContadorPageState createState() => _ContadorPageState();
